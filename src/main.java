1df3547f7cbd7d4fcdc6d8c0af2822de41fc435e
Neto Aparovich <aparovich.gregory@gmail.com>
2023-01-15 16:39:08 +0300

    first

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+
+    public static void main(String[] args) {
+        int price = 2000;
+        int bonus = price / 20;
+        System.out.println("Начисленно " + (bonus) + " бонусных(ая) миль(я)");
+
+
+    }
+}
