f3deb09946073b45565db45cd1abeb59a8fdaef6
Neto Aparovich <aparovich.gregory@gmail.com>
2023-01-20 20:22:41 +0300

    first

--- src/main.java
+++ src/main.java
@@ -1,9 +1,11 @@
 public class main {
 
     public static void main(String[] args) {
-        int price = 2000;
-        int bonus = price / 20;
-        System.out.println("Начисленно " + (bonus) + " бонусных(ая) миль(я)");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles;
+        miles = service.calculate(price);
+        System.out.println(miles);
 
 
     }
